e01e6556f4a0baa07ffa368d3eaf0606be741a0f
Ennio Visconti <ennio.visconti@gmail.com>
2024-02-15 15:50:13 +0100

    restored project

---
+++ build.gradle.kts
@@ -0,0 +1,98 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "tuwien.cps"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    // Kotlin support
+    kotlin("jvm") version "1.9.22"
+
+    // Package publishing
+    `maven-publish`
+
+    // CLI runnable
+    application
+
+    // Dependency analysis
+    id("org.kordamp.gradle.jdeps") version "0.20.0"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "bayesformal"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Bayes-Formal Experiment")
+                description.set("An experiment to explore a bayes-formal approach to smart city analysis")
+                url.set("https://zk35.org/")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ennioVisco")
+                        name.set("Ennio Visconti")
+                        email.set("ennio.visconti@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/ennioVisco/bayesformal.git")
+                    developerConnection.set("scm:git:ssh://github.com/ennioVisco/bayesformal.git")
+                    url.set("https://github.com/ennioVisco/bayesformal/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ennioVisco/bayesformal")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation(files("lib/moonlight.jar"))
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.10")
+    implementation("org.slf4j:slf4j-simple:1.7.29")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    testImplementation(kotlin("test"))
+    compileOnly("org.kordamp.gradle:jdeps-gradle-plugin:0.15.0")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "21"
+}
+
+application {
+    // we take in the -Pmain=CLASS_NAME the relative classname to run
+    // if empty, we run the `Erlang` class
+    val exec = project.findProperty("main")?.toString() ?: "Erlang"
+    fun pkg(name: String) = "at.ac.${group}.${name}Kt"
+    mainClass.set(pkg(exec))
+}
